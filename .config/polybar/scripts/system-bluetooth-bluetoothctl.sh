4cd1d2fbf1e9f7d21a0779060268082f966ec362
Baptiste Beduneau <baptiste@beduneau.com>
2021-10-15 20:46:50 +0200

    Add bluetooth and spotify to polybar

---
+++ .config/polybar/scripts/system-bluetooth-bluetoothctl.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+bluetooth_print() {
+    bluetoothctl | while read -r; do
+        if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
+            echo ''
+
+            devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+
+            for device in $devices_paired; do
+                device_info=$(bluetoothctl info "$device")
+
+                if echo "$device_info" | grep -q "Connected: yes"; then
+                    device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+
+                    echo ''
+
+                fi
+            done
+        else
+            echo ""
+        fi
+    done
+}
+
+bluetooth_toggle() {
+    if bluetoothctl show | grep -q "Powered: no"; then
+        bluetoothctl power on >> /dev/null
+        sleep 1
+
+        devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+        echo "$devices_paired" | while read -r line; do
+            bluetoothctl connect "$line" >> /dev/null
+        done
+    else
+        devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
+        echo "$devices_paired" | while read -r line; do
+            bluetoothctl disconnect "$line" >> /dev/null
+        done
+
+        bluetoothctl power off >> /dev/null
+    fi
+}
+
+case "$1" in
+    --toggle)
+        bluetooth_toggle
+        ;;
+    *)
+        bluetooth_print
+        ;;
+esac
